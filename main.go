bcfc681dcc56718dd935c9a98649b68ffc7ece21
Izaak Dale <izaakdaledev@gmail.com>
2024-06-27 20:15:35 -0700

    add deploy

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"net/http"
+)
 
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+	http.ListenAndServe(":80", mux)
 }
